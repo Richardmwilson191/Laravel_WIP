0b430468108da146b03787a6b0b1ee7edfde882e
Rajeime <rajeime.m.roberts@gmail.com>
2021-09-27 00:04:56 -0500

    Made the user redirect to their respective dashbored based on their roles.

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
         {
             return redirect(route('Dashboard'));
         }
-        elseif(auth()->user() != NULL and auth()->user()->is_admin == True)
+        elseif(auth()->user() != NULL and auth()->user()->is_admin)
         {
             return view("admin.index");
         }
